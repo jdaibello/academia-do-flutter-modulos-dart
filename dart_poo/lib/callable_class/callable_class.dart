6bc9287c792085c1302dc5028e42f1e154bc92c6
João Pedro <jpedrodaibello1999@outlook.com>
2021-10-16 22:37:02 -0300

    feat: add callable class

---
+++ dart_poo/lib/callable_class/callable_class.dart
@@ -0,0 +1,10 @@
+import 'package:dart_poo/callable_class/enviar_email.dart';
+
+void main() {
+  var enviarEmail = EnviarEmail();
+
+  // Chamar a classe como se fosse uma função
+  enviarEmail('rodrigorahman@academiadoflutter.com.br');
+
+  // enviarEmail.enviar('rodrigorahman@academiadoflutter.com.br');
+}
