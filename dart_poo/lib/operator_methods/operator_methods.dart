c76ea24f07af32f7d8bf0efbe407c7209f4a28b1
João Pedro <jpedrodaibello1999@outlook.com>
2021-10-16 22:48:07 -0300

    feat: add operator methods

---
+++ dart_poo/lib/operator_methods/operator_methods.dart
@@ -0,0 +1,12 @@
+import 'package:dart_poo/operator_methods/numero.dart';
+
+void main() {
+  var num1 = Numero(10);
+  var num2 = Numero(20);
+
+  var totalSoma = num1 + num2;
+  print(totalSoma.i);
+
+  var totalSub = num1 - num2;
+  print(totalSub.i);
+}
