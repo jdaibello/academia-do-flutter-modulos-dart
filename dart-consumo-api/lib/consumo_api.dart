cece21d512fe6003df131aba8676532c5e3f59d5
João Pedro <jpedrodaibello1999@outlook.com>
2022-11-23 08:59:48 -0300

    feat: working with JSON in Dart

--- dart-consumo-api/lib/consumo_api.dart
+++ dart-consumo-api/lib/consumo_api.dart
@@ -1,1 +1,43 @@
-void main() {}
+import 'dart:convert';
+
+void main() {
+  final cidadeJson =
+      '''
+      [
+        {
+          "id": 1,
+          "nome": "São Paulo",
+          "regiao": {
+            "nome": "Sudeste"
+          }
+        },
+        {
+          "id": 2,
+          "nome": "Fortaleza",
+          "regiao": {
+            "nome": "Nordeste"
+          }
+        }
+      ]
+      ''';
+
+  //* [] => List
+  //* {} => Map<String, dynamic>
+
+  final cidadeMap = json.decode(cidadeJson);
+
+  if (cidadeMap is List) {
+    print('É uma lista');
+  } else if (cidadeMap is Map) {
+    print('É um map');
+  }
+
+  print(cidadeMap.runtimeType);
+  cidadeMap.forEach((cidade) => print(cidade['regiao']['nome']));
+  print(cidadeMap[0].runtimeType);
+
+  final cidadeMapJson = {'id': 123, 'nome': 'Santo André', 'uf': 'SP'};
+
+  // [{"id":123,"nome":"Santo André","uf":"SP"}]
+  print(json.encode([cidadeMapJson]));
+}
